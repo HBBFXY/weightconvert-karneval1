88e3fad32cb914391b7463f0b57218f0f8ece317
karneval1 <2128016686@qq.com>
2025-09-09 15:46:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if weightStr[-2:]in['kg']:
   pd=eval(weightStr[0:-2])*2.2046
   print("转换后的英制重量是{:.3f}pd".format(pd))
 elif weightStr[-2:]in['pd']:
-  kg=eval(weightStr[0:-2])/2.2026
+  kg=eval(weightStr[0:-2])/2.2046
   print("转换后的公制重量是{:.3f}kg".format(kg))
 else:
   print("输入格式错误")
